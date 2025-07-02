607f5a17566550e4dcf07ec9366899310ad040e8
Sammed Sagare <76863715+sammedsagare@users.noreply.github.com>
2025-07-02 12:00:38 +0530

    added huggingface embedding and vector store using faiss

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 from langchain_community.document_loaders import PyMuPDFLoader
 from pathlib import Path
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
 
-pdf_path = Path("constitution_of_india.pdf")
-loader = PyMuPDFLoader(str(pdf_path))
-documents = loader.load()
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-print(f"✅ Loaded {len(documents)} page(s) from: {pdf_path.name}")
 
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=800,
-    chunk_overlap=200,
-    separators=["\n\n", "\n", ".", ";", ",", " "],
-)
+def vector_db_from_pdf(pdf_path: str, index_save_path: str = "faiss_index") -> FAISS:
+    
+    loader = PyMuPDFLoader(str(pdf_path))
+    documents = loader.load()
+    print(f"✅ Loaded {len(documents)} page(s) from: {pdf_path}")    
+    
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,        
+        chunk_overlap=200,     
+        separators=["\n\n", "\n", ".", ";", ",", " "],  
+    )
+    
+    chunks = splitter.split_documents(documents)
+    print(f"✅ Split into {len(chunks)} chunks.\n")
+    
+    vectorstore = FAISS.from_documents(chunks, embeddings)
+    vectorstore.save_local("faiss_index")
+    print("✅ FAISS index created and saved.")
+    
+    return vectorstore
 
-chunks = splitter.split_documents(documents)
-print(f"✅ Split into {len(chunks)} chunks.\n")
+if __name__ == "__main__":
+    pdf_file = Path("constitution_of_india.pdf")
+    vector_db_from_pdf(str(pdf_file))
