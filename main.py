a4a4c9da1007535b5ffbaf080c9f2c8bbcbaad4b
Sammed Sagare <76863715+sammedsagare@users.noreply.github.com>
2025-07-02 11:45:25 +0530

    changed pdf & added chunk size and overlap optimization using RecursiveCharacterTextSplitter

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from langchain_community.document_loaders import PyMuPDFLoader
 from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-pdf_path = Path("aies.pdf")
+
+pdf_path = Path("constitution_of_india.pdf")
 loader = PyMuPDFLoader(str(pdf_path))
 documents = loader.load()
 
 print(f"✅ Loaded {len(documents)} page(s) from: {pdf_path.name}")
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=800,
+    chunk_overlap=200,
+    separators=["\n\n", "\n", ".", ";", ",", " "],
+)
+
+chunks = splitter.split_documents(documents)
+print(f"✅ Split into {len(chunks)} chunks.\n")
